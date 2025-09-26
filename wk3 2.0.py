edff5e1e5a6444d0ce3365cf22bff0c03b70290e
ryanduggan-art <ryan.duggan@hawkeyecollege.edu>
2025-09-26 10:23:11 -0500

    Practice change: added test print line

--- wk3 2.0.py
+++ wk3 2.0.py
@@ -7,3 +7,4 @@ create_a_list = ['The', 'name', 'entered', 'is'] + [name_input]
 get_length_of_list = (len(name_input) + integer_additive)
 print(create_a_list)
 print(get_length_of_list)
+print("This is a test change for Git practice")
